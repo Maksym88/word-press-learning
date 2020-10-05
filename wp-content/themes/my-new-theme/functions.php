5707f29e022e399a200e993d87fdffe7a1a4997c
Maksym Koniaiev <maksym.konyaev@gmail.com>
2020-10-05 13:19:46 +0300

    Create post type

--- wp-content/themes/my-new-theme/functions.php
+++ wp-content/themes/my-new-theme/functions.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Included styles bootstrap
+ */
 function add_theme_scripts() {
 
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/deps/bootstrap/css/bootstrap.css', false, '4.5.2', 'all' );
@@ -9,6 +12,10 @@ function add_theme_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
+
+/**
+ * Created new test sidebar
+ */
 function my_register_sidebars() {
 	register_sidebar(
 		array(
@@ -25,6 +32,9 @@ function my_register_sidebars() {
 
 add_action( 'widgets_init', 'my_register_sidebars' );
 
+/**
+ * created new test nav menu
+ */
 function my_register_menus() {
 	register_nav_menus(
 		array(
@@ -35,3 +45,22 @@ function my_register_menus() {
 }
 
 add_action( 'init', 'my_register_menus' );
+
+/**
+ * Created post type by Training
+ */
+function my_register_post() {
+	register_post_type( 'trainings',
+		array(
+			'labels'      => array(
+				'name'          => __( 'Training' ),
+				'singular_name' => __( 'Training' ),
+			),
+			'supports'    => array( 'title', 'editor', 'thumbnail', 'post-formats', 'excerpt' ),
+			'public'      => true,
+			'has_archive' => true
+		)
+	);
+}
+
+add_action( 'init', 'my_register_post' );
